9e3a006c9970f0e5e55584a9f988e4bf0b226f8e
Administrator <526382015@qq.com>
2022-06-15 11:03:14 +0800

    20220615

--- spring-badee/src/main/java/cofnig/AppConfig.java
+++ spring-badee/src/main/java/cofnig/AppConfig.java
@@ -1,2 +1,18 @@
-package cofnig;public class AppConfig {
+package cofnig;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+
+@Configuration
+public class AppConfig {
+	@Bean
+	public  MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
+
--- spring-badee/src/main/java/service/UserService.java
+++ spring-badee/src/main/java/service/UserService.java
@@ -1,4 +1,4 @@
-package servcie;
+package service;
 
 import org.springframework.stereotype.Service;
 
